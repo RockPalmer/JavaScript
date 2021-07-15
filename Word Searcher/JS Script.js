6ee668552316e38d2fa7edd2c25827819cb6fd97
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2021-07-15 11:49:50 -0700

    Added 2 new options
    User can 1) search only for instances of the term that aren't surrounded by other letters and 2) search for the term without being case specific

--- Word Searcher/JS Script.js
+++ Word Searcher/JS Script.js
@@ -1,42 +1,162 @@
 function myfunc()
 {
+    let withoutLetters = document.getElementById("check1").checked;
+    let caseSpecific = document.getElementById("check2").checked;
     let text = document.getElementById("area").value;
     let word = document.getElementById("in").value;
-    let count2 = 0;
     let indexes = new Array(0);
-    let linenum = 1;
-    let charIndex = 1;
-    for (let count1 = 0; count1 < text.length; count1++)
+    let charIndex = 0;
+    let textArr = new Array(0);
+    let line = '';
+    /*Put each line of text into an array*/
+    for (let count = 0; count < text.length; count++)
     {
-        if (text.charAt(count1) != '\n')
+        if (text.charAt(count) !== '\n')
         {
-            if (text.charAt(count1) == word.charAt(count2))
-            {
-                count2++;
-                if (count2 == word.length)
-                {
-                    index = [linenum,(charIndex - word.length + 1)];
-                    indexes.push(index);
-                    count2 = 0;
-                }
-            }
-            else
-            {
-                count2 = 0;
-            }
+            line = line + text.charAt(count);
         }
         else
         {
-            linenum++;
-            charIndex = 0;
+            textArr.push(line);
+            line = '';
         }
-        charIndex++;
     }
-    let out = "Entered phrase appears at\n";
-    for (let count = 0; count < indexes.length - 1; count++)
+    textArr.push(line);
+    /*************************************/
+
+    /*Search textArr for the word and store each instance in indexes*/
+    if (caseSpecific)
     {
-        out += "line " + indexes[count][0]  + ", character " + indexes[count][1] + ",\n"
+        for (let count1 = 0; count1 < textArr.length; count1++)
+        {
+            for (let count2 = 0; count2 < textArr[count1].length; count2++)
+            {
+                if (textArr[count1].charAt(count2) === word.charAt(charIndex))
+                {
+                    charIndex++;
+                    if (charIndex === word.length)
+                    {
+                        indexes.push([count1, count2 - word.length + 1]);
+                        charIndex = 0;
+                    }
+                }
+                else
+                {
+                    charIndex = 0;
+                }
+            }
+        }
     }
-    out += "and line " + indexes[indexes.length - 1][0] + ", character " + indexes[indexes.length - 1][1] + ".\n"
-    document.getElementById("output").innerHTML = out;
+    else
+    {
+        for (let count1 = 0; count1 < textArr.length; count1++)
+        {
+            for (let count2 = 0; count2 < textArr[count1].length; count2++)
+            {
+                let code = textArr[count1].charCodeAt(count2);
+                if (textArr[count1].charAt(count2) === word.charAt(charIndex))
+                {
+                    charIndex++;
+                    if (charIndex === word.length)
+                    {
+                        indexes.push([count1, count2 - word.length + 1]);
+                        charIndex = 0;
+                    }
+                }
+                else if ((code > 64 && code < 91) || (code > 96 && code < 123))
+                {
+                    let thisChar;
+                    if (code < 96)
+                    {
+                        thisChar = String.fromCharCode(code + 32);
+                    }
+                    else
+                    {
+                        thisChar = String.fromCharCode(code - 32);
+                    }
+                    if (thisChar === word.charAt(charIndex))
+                    {
+                        charIndex++;
+                        if (charIndex === word.length)
+                        {
+                            indexes.push([count1, count2 - word.length + 1]);
+                            charIndex = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    charIndex = 0;
+                }
+            }
+        }
+    }
+    /****************************************************************/
+
+    /*Remove any instances where the word is surrounded by letters*/
+    if (withoutLetters)
+    {
+        let badIndexes = new Array(0);
+        for (let count = 0; count < indexes.length; count++)
+        {
+            let lineIndex = indexes[count][0];
+            charIndex = indexes[count][1];
+            let isValid = true;
+            if (charIndex > 0)
+            {
+                let c = textArr[lineIndex].charCodeAt(charIndex - 1);
+                if ((c > 64 && c < 91) || (c > 96 && c < 123))
+                {
+                    isValid = false;
+                }
+            }
+            charIndex = charIndex + word.length;
+            if (isValid && charIndex < textArr[lineIndex].length)
+            {
+                let c = textArr[lineIndex].charCodeAt(charIndex);
+                if ((c > 64 && c < 91) || (c > 96 && c < 123))
+                {
+                    isValid = false;
+                }
+            }
+
+            if (!isValid)
+            {
+                badIndexes.push(count);
+            }
+        }
+        for (let x = 0; x < badIndexes.length; x++)
+        {
+            indexes.splice(badIndexes[x], 1);
+        }
+    }
+    /*************************************************************/
+
+    /*Adjust all line and char number values*/
+    for (let x = 0; x < indexes.length; x++)
+    {
+        indexes[x].splice(0,1,indexes[x][0] + 1)
+        indexes[x].splice(1,1,indexes[x][1] + 1)
+    }
+    /****************************************/
+
+    try
+    {
+        let out = "Entered phrase appears at\n";
+        for (let count = 0; count < indexes.length - 1; count++)
+        {
+            out += "line " + indexes[count][0]  + ", character " + indexes[count][1] + ",\n"
+        }
+        out += "and line " + indexes[indexes.length - 1][0] + ", character " + indexes[indexes.length - 1][1] + ".\n"
+        document.getElementById("output").innerHTML = out;
+    }
+    catch (e)
+    {
+        let out = "An error occurred. Please try again";
+        if (e instanceof TypeError)
+        {
+            out = "Entered phrase not found\n";
+        }
+        document.getElementById("output").innerHTML = out;
+    }
 }
